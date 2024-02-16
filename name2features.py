b57c053ea767fa7353d05acc28314f2c81bf0f52
QianmeiYang <158218110+QianmeiYang@users.noreply.github.com>
2024-02-15 18:01:45 -0600

    Update name2features.py

--- name2features.py
+++ name2features.py
@@ -10,7 +10,7 @@ def name2features(name):
     But every name must generate the same length feature. 
     """
     
-    d = 4096 # number of hashing buckets
+    d = 32768 # number of hashing buckets
     v = np.zeros(d)
     name=name.lower() 
     
